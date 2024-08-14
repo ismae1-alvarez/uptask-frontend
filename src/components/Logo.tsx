a93b1cfbc0663cb18d493744dbf238d5ca37e231
Ismael Alvarez <ismaelalvarez514@gmail.com>
2024-08-14 00:56:42 -0600

    Initial project, front uptask

---
+++ src/components/Logo.tsx
@@ -0,0 +1,6 @@
+function Logo() {
+  return (
+    <img src="./logo.svg" alt="logotipo Uptask" />
+  )
+}
+export default Logo
---
+++ src/views/DashboardView.tsx
@@ -0,0 +1,6 @@
+const DashboardView = () => {
+  return (
+    <div>DashboardView</div>
+  )
+}
+export default DashboardView
