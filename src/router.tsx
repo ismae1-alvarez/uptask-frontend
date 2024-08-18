7f53debfbcbb20a1006926dd10e005e9ec29935a
Ismael Alvarez <ismaelalvarez514@gmail.com>
2024-08-17 20:39:09 -0600

    Disignt forms login and createuser

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,9 @@ import DashboardView from "@/views/DashboardView"
 import CreateProjecttView from "./views/projetcs/CreateProjectView"
 import EditProjectView from "./views/projetcs/EditProjectView"
 import ProjectDetailsView from "./views/projetcs/ProjectDetailsView"
+import AuthLayout from "./layouts/AuthLayout"
+import LoginView from "./views/auth/LoginView"
+import RegisterView from "./views/auth/RegisterView"
 
 const Router = () => {
   return (
@@ -14,6 +17,11 @@ const Router = () => {
                 <Route path="/projects/create" element={<CreateProjecttView/>}/>
                 <Route path="/projects/:projectId" element={<ProjectDetailsView/>}/>
                 <Route path="/projects/:projectId/edit" element={<EditProjectView/>}/>
+            </Route>
+
+            <Route element={<AuthLayout/>}>
+              <Route path="/auth/login" element={<LoginView/>}/>
+              <Route path="/auth/register" element={<RegisterView/>}/>
             </Route>
         </Routes>
     </BrowserRouter>
