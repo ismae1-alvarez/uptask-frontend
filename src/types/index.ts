687c355c2f26548c23ba34fb2c752fe31eb427e1
Ismael Alvarez <ismaelalvarez514@gmail.com>
2024-08-20 22:46:49 -0600

    Integrate function, update password and personal data

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,6 +6,7 @@ const authSchema = z.object({
     name : z.string(),
     email : z.string().email(),
     password : z.string(),
+    current_password : z.string(),
     password_confirmation : z.string(),
     token: z.string(),
 });
@@ -16,6 +17,7 @@ export type UserRegistrationForm = Pick<Auth, 'name' | 'email' | 'password' | 'p
 export type RequestConfirmationCodeForm = Pick<Auth,  'email' >;
 export type ForgotPasswordForm = Pick<Auth,  'email' >;
 export type NewPasswordForm = Pick<Auth,  'password' | 'password_confirmation' >;
+export type UpdateCurrentUserPasswordForm = Pick<Auth,  'current_password' |'password' | 'password_confirmation' >;
 export type ConfirmToken = Pick<Auth, 'token'>;
 
 /** Users */
@@ -28,6 +30,7 @@ export const userSchema = authSchema.pick({
 });
 
 export type User =  z.infer<typeof userSchema>;
+export type UserProfileForm = Pick<User, 'name' | 'email'>;
 
 /** Notes */
 const noteSchema =  z.object({
